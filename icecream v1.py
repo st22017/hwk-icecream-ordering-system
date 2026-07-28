913599ce472c498c3090a62b240ea277f6460f42
st22017 <st22017@howick.school.nz>
2026-07-29 10:49:49 +1200

    Main program
    Refined GUI, added basic functionality for ordering.

--- icecream v1.py
+++ icecream v1.py
@@ -1,73 +1,77 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 #constants
 TITLE_FONT = "Comic Sans MS", 35
+TEXT_FONT = "Comic Sans MS", 15
 
 cones = ["Sugar", "Waffle", "Cake"]
 flavours = ["Vanilla", "Chocolate", "Strawberry", "Hokey Pokey"]
 toppings = ["Chocolate", "Chocolate Flake", "Cherry", "Sprinkles"]
 
-def ConfirmOrder():
-    cone = conesl.get()
-    flavour = flavoursl.get()
-    toppings = toppingsl.get()
+class OrderGui:
+    """gui setup"""
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Ordering Program")
+        self.root.geometry ("1280x720")
+        self.root.resizable(0, 0)
 
-    print(cone)
-    print(flavour)
-    print(toppings)
-    pass
+        self.root.columnconfigure([0, 1, 2, 3], weight=1)
+        self.root.rowconfigure([0, 1, 2], weight=1)
 
+        # gui layout
+        self.title = tk.Label(root, text="Howick Icy Treats", bg="lightblue", font=TITLE_FONT)
+        self.title.grid(row=0, column=0, sticky="nsew", columnspan=3)
+
+        self.orderlb = tk.Label(root, text="Your Order:", bg="skyblue", font=TEXT_FONT)
+        self.orderlb.grid(row=0, column=3, sticky="nsew")
+
+        self.img1 = tk.PhotoImage(file="wafflecone.png")
+        self.img1lb = tk.Label(root, image=self.img1, width=22)
+        self.img1lb.grid(row=1, column=0, sticky="nsew", rowspan = 2)
+
+        self.img2 = tk.Label(root, text="Image Placeholder", bg="lightgreen", width=22)
+        self.img2.grid(row=1, column=1, sticky="nsew", rowspan = 2)
+
+        self.img3 = tk.Label(root, text="Image Placeholder", bg="lightgreen", width=22, height=25)
+        self.img3.grid(row=1, column=2, sticky="nsew", rowspan = 2)
+
+        self.conelb = tk.Label(root, text="Cone", bg="skyblue", font=TEXT_FONT)
+        self.conelb.grid(row=3, column=0, sticky="nsew")
+
+        self.flavourlb = tk.Label(root, text="Flavour", bg="skyblue", font=TEXT_FONT)
+        self.flavourlb.grid(row=3, column=1, sticky="nsew")
+
+        self.toppinglb = tk.Label(root, text="Topping", bg="skyblue", font=TEXT_FONT, height=3)
+        self.toppinglb.grid(row=3, column=2, sticky="nsew")
+
+        self.conesl = ttk.Combobox(root, values=cones, state="readonly")
+        self.conesl.grid(row=4, column=0, sticky="nsew")
+
+        self.flavoursl = ttk.Combobox(root, values=flavours, state="readonly")
+        self.flavoursl.grid(row=4, column=1, sticky="nsew")
+
+        self.toppingsl = ttk.Combobox(root, values=toppings, state="readonly")
+        self.toppingsl.grid(row=4, column=2, sticky="nsew", ipady = 5)
+
+        self.display = tk.Label(root, text="(placeholder)", bg="lightblue")
+        self.display.grid(row=1, column=3, sticky="nsew", rowspan=2)
+
+        self.confirmbtn = tk.Button(root, text="Confirm Order", bg="skyblue", font=TEXT_FONT, command=self.ConfirmOrder)
+        self.confirmbtn.grid(row=3, column=3, sticky="nsew", rowspan=2)
+    
+    def ConfirmOrder(self):
+        cone = self.conesl.get()
+        flavour = self.flavoursl.get()
+        toppings = self.toppingsl.get()
+
+        print(cone)
+        print(flavour)
+        print(toppings)
+        pass
 
 root = tk.Tk()
-root.title("Ordering Program")
-root.geometry ("1280x720")
-root.resizable(0, 0)
-
-root.columnconfigure([0, 1, 2, 3], weight=1)
-root.rowconfigure([0, 1, 2], weight=1)
-
-# gui layout
-title = tk.Label(root, text="Howick Icy Treats", bg="lightblue", font=TITLE_FONT)
-title.grid(row=0, column=0, sticky="nsew", columnspan=3)
-
-orderlb = tk.Label(root, text="Your Order:", bg="skyblue")
-orderlb.grid(row=0, column=3, sticky="nsew")
-
-img1 = tk.Label(root, text="Image Placeholder", bg="lightgreen")
-img1.grid(row=1, column=0, sticky="nsew", rowspan = 2)
-
-img2 = tk.Label(root, text="Image Placeholder", bg="lightgreen")
-img2.grid(row=1, column=1, sticky="nsew", rowspan = 2)
-
-img3 = tk.Label(root, text="Image Placeholder", bg="lightgreen", height=25)
-img3.grid(row=1, column=2, sticky="nsew", rowspan = 2)
-
-conelb = tk.Label(root, text="Cone", bg="skyblue")
-conelb.grid(row=3, column=0, sticky="nsew")
-
-flavourlb = tk.Label(root, text="Flavour", bg="skyblue")
-flavourlb.grid(row=3, column=1, sticky="nsew")
-
-toppinglb = tk.Label(root, text="Topping", bg="skyblue", height=5)
-toppinglb.grid(row=3, column=2, sticky="nsew")
-
-conesl = ttk.Combobox(root, values=cones, state="readonly")
-conesl.grid(row=4, column=0, sticky="nsew")
-
-flavoursl = ttk.Combobox(root, values=flavours, state="readonly")
-flavoursl.grid(row=4, column=1, sticky="nsew")
-
-toppingsl = ttk.Combobox(root, values=toppings, state="readonly")
-toppingsl.grid(row=4, column=2, sticky="nsew", ipady = 5)
-
-display = tk.Label(root, text="(placeholder)", bg="lightblue")
-display.grid(row=1, column=3, sticky="nsew", rowspan=2)
-
-confirmbtn = tk.Button(root, text="Confirm Order", bg="skyblue", command=lambda:ConfirmOrder())
-confirmbtn.grid(row=3, column=3, sticky="nsew", rowspan=2)
-
-print("test")
-
+app = OrderGui(root)
 root.mainloop()
-
